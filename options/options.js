c415fb032cc3a8d8068e0fddf2b660b06b9ea513
Crowdbotics <team@crowdbotics.com>
2021-10-21 17:35:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ancient_grass_31623",
+  url: "https://ancient_grass_31623.botics.co",
+  api: "https://ancient_grass_31623.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
